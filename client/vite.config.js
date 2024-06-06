e85b685c53c56801618e653350de257f57d64cd2
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-06 07:35:41 -0400

    server port testing

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3001,
+    port: 3001,
     open: true,
     proxy: {
       '/graphql': {
-        target: `http://localhost:${process.env.PORT + 1 || 3001}`,
+        target: `http://localhost:3001`,
         secure: false,
         changeOrigin: true,
       },
