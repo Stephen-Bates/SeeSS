d04b60553e91d749efe0378d1d66ba1a13e032e1
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-06 22:02:47 -0400

    updated client with minimum requirements for PWA. added icons and metadata to index.html. Added vite-plugin-pwa as dependancy and implemented it in vite.confige.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,40 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
 import 'dotenv/config'
 
 
 // https://vitejs.dev/config/
-
 console.log(`starting client server at ${(process.env.PORT || 3000) + 1}`);
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'See Style Sheets',
+        short_name: 'SeeSS',
+        description: 'Build, Share, and Learn CSS',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      },
+    }),
+    react()
+  ],
   server: {
     port: (process.env.PORT || 3000) + 1,
     open: true,
